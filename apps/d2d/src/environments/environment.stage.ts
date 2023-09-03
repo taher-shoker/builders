89f27790318d2d311f4eb3be0d0228db89c385d9
taher shoker <taher.shoker@qeema.net>
2023-09-03 16:19:47 +0300

    create new environment as stage

--- apps/d2d/src/environments/environment.stage.ts
+++ apps/d2d/src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8090/api/fm',
+  production: true,
+  apiUrl: 'http://localhost:8091/api/fm',
   authUrl: 'http://localhost:9084',
 };
