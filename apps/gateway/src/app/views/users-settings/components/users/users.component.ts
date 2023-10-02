c49ceb8387e7e032d64c33c56aa1b8639b565b7e
taher shoker <taher.shoker@qeema.net>
2023-10-02 12:13:32 +0300

    fixing filtration issue

--- apps/gateway/src/app/views/users-settings/components/users/users.component.ts
+++ apps/gateway/src/app/views/users-settings/components/users/users.component.ts
@@ -160,10 +160,18 @@ export class UsersComponent implements OnInit, AfterViewInit {
   handleSelectChange(value: any, dropDwonType: string) {
     if (dropDwonType === 'team') {
       if (value.id === 'all') {
-        this.dataSource.data = this.list;
+        if (this.selectedPrivilege.id !== 0) {
+          this.dataSource.data = this.list.filter(
+            (x: any) =>
+              this.userService.getUserPrivilege(x) ===
+              this.selectedPrivilege.groupName
+          );
+        } else {
+          this.dataSource.data = this.list;
+        }
       } else {
         this.dataSource.data = this.list.filter(
-          (x: any) => this.userService.getUserTeam(x) == value?.name
+          (x: any) => this.userService.getUserTeam(x) === value?.name
         );
       }
     } else {
@@ -174,6 +182,8 @@ export class UsersComponent implements OnInit, AfterViewInit {
         this.dataSource.data = this.list;
         this.getTeams();
       } else {
+        this.selectedPrivilege = value;
+
         this.teams = this.userService
           .getTeams()
           .filter((x: any) => x.roleName == value.groupName);
