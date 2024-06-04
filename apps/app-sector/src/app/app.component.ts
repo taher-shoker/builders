010ba3af5f0ac63715e0aaf94f49265cbc244e6e
SaraAlkurdy <sara.alkurdy@gmail.com>
2024-06-04 11:28:19 +0300

    init app.

--- apps/app-sector/src/app/app.component.ts
+++ apps/app-sector/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie';
 
 @Component({
   selector: 'stc-apps-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-sector';
+
+  constructor(
+    private cookieService: CookieService,
+    public router: Router
+  ) {}
+
+  urlHome = '/home';
+  userName = '';
+  logoSrc = 'assets/images/brand/stc-logo.png';
+  sidebarLogoSrc = 'assets/images/brand/sidebar-logo.png';
+  navItems = [
+    {
+      name: 'home',
+      url: '/home',
+      icon: 'fa-home',
+      roles: ['APPROVERS,CREATORS'],
+      urlHome: '/home',
+    },
+    {
+      name: 'dashboard',
+      url: '/dashboard',
+      icon: 'fa-chart-line',
+      roles: ['APPROVERS'],
+      urlHome: '/home',
+    },
+  ];
+
+  ngOnInit() {
+    this.userName = this.cookieService.get('USER_FULLNAME') || '';
+  }
+
+  backToHome() {
+    this.router.navigate(['/']);
+  }
+  logOut() {
+    //apply logout action
+    }
 }
--- apps/app-sector/src/app/app.module.ts
+++ apps/app-sector/src/app/app.module.ts
@@ -1,13 +1,48 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { SharedUiModule } from '@stc-apps/shared-ui';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from '../environments/environment';
+import { ToastrModule } from 'ngx-toastr';
+import { CookieModule } from 'ngx-cookie';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, environment.languageFilesPath, '.json');
+}
+
+export const provideTranslation = () => ({
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+});
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    SharedUiModule,
+    TranslateModule,
+    ToastrModule.forRoot(),
+    CookieModule.withOptions(),
+  ],
+    providers: [
+      importProvidersFrom([
+        HttpClientModule,
+        TranslateModule.forRoot(provideTranslation()),
+      ]),
+     
+    ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
