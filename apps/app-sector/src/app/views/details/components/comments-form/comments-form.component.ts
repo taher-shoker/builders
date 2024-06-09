b59441a5be7cf9b7f2caa3cc6e16f4542c239693
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-06-09 16:47:28 +0300

    add comments-form component.

---
+++ apps/app-sector/src/app/views/details/components/comments-form/comments-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stc-apps-comments-form',
+  templateUrl: './comments-form.component.html',
+  styleUrls: ['./comments-form.component.scss']
+})
+export class CommentsFormComponent {
+
+}
