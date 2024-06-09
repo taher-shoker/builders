bd84669fd0128911666ca65b7ce4588fb92cb1a2
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-06-09 16:49:43 +0300

    add contents to details component.

--- apps/app-sector/src/app/views/details/details.module.ts
+++ apps/app-sector/src/app/views/details/details.module.ts
@@ -4,8 +4,10 @@ import { appRoutes } from './details.routes';
 import { DetailsComponent } from './details.component';
 import { SharedModule } from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { DetailsCardComponent } from './components/details-card/details-card.component';
+import { CommentsFormComponent } from './components/comments-form/comments-form.component';
 
-const components = [DetailsComponent];
+const components = [DetailsComponent, DetailsCardComponent, CommentsFormComponent];
 const modules = [SharedModule, RouterModule.forRoot(appRoutes)];
 
 @NgModule({
