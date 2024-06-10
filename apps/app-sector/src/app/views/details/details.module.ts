396e88f4bc65ed9600697efc0f6f9f26d35aad61
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-06-10 14:34:57 +0300

    add reactive form in comments-form component.

--- apps/app-sector/src/app/views/details/details.module.ts
+++ apps/app-sector/src/app/views/details/details.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { DetailsCardComponent } from './components/details-card/details-card.component';
 import { CommentsFormComponent } from './components/comments-form/comments-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const components = [DetailsComponent, DetailsCardComponent, CommentsFormComponent];
-const modules = [SharedModule, RouterModule.forRoot(appRoutes)];
+const modules = [SharedModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes)];
 
 @NgModule({
   declarations: [...components],
