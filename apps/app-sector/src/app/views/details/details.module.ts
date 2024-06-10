20f8d5b05eee08daace95ad1bea517021c0eb340
Habiba Mohamed <habiba.mohamed@qeema.net>
2024-06-10 13:59:31 +0300

    solving merge issue

--- apps/app-sector/src/app/views/details/details.module.ts
+++ apps/app-sector/src/app/views/details/details.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 import { DetailsCardComponent } from './components/details-card/details-card.component';
 import { CommentsFormComponent } from './components/comments-form/comments-form.component';
 
-const components = [DetailsComponent];
+const components = [DetailsComponent,CommentsFormComponent,DetailsCardComponent];
 const modules = [SharedModule, RouterModule.forChild(appRoutes)];
 
 @NgModule({
