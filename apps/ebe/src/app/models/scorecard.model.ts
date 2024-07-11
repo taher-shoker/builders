5bc1ac10dec9f25db4c3b85a7059ae659f78713f
Sherif <Sherif.Helmy@qeema.net>
2024-07-11 13:10:50 +0300

    working on the design of the scorecard page

---
+++ apps/ebe/src/app/models/scorecard.model.ts
@@ -0,0 +1,5 @@
+export interface ScorecardTaps
+{
+  id : number;
+  name : string;
+}
--- apps/ebe/src/app/views/scorecard/scorecard.component.ts
+++ apps/ebe/src/app/views/scorecard/scorecard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScorecardTaps } from '../../models/scorecard.model';
 
 @Component({
   selector: 'stc-apps-scorecard',
@@ -8,4 +9,37 @@ import { CommonModule } from '@angular/common';
   templateUrl: './scorecard.component.html',
   styleUrl: './scorecard.component.scss',
 })
-export class ScorecardComponent {}
+export class ScorecardComponent {
+  currentClickedTapIndex = 0;
+  currentClickedTapData:ScorecardTaps;
+  scorecardsTaps:ScorecardTaps[] = [
+    {
+      id : 1,
+      name : "financial"
+    },
+    {
+      id : 2,
+      name : "strategic"
+    },
+    {
+      id : 3,
+      name : "relational"
+    },
+    {
+      id : 4,
+      name : "operational"
+    },
+    {
+      id : 5,
+      name : "corporate priorities"
+    },
+  ];
+  constructor(){
+    this.currentClickedTapData = this.scorecardsTaps[0];
+  }
+  toggleTaps(index:number , tap:ScorecardTaps)
+  {
+    this.currentClickedTapIndex = index;
+    this.currentClickedTapData = tap;
+  }
+}
