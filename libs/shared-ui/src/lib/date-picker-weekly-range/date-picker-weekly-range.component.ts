f4bc748c40a3edbc23a3357e9b81f83fba8111b9
taher shoker <taher.shoker@qeema.net>
2024-07-20 16:50:18 +0300

    fix tslint error

--- libs/shared-ui/src/lib/date-picker-weekly-range/date-picker-weekly-range.component.ts
+++ libs/shared-ui/src/lib/date-picker-weekly-range/date-picker-weekly-range.component.ts
@@ -2,7 +2,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 /* eslint-disable @typescript-eslint/no-inferrable-types */
-import moment from 'moment';
+import * as moment from 'moment';
+
 import {
   YearRangeObj,
   YearObj,
