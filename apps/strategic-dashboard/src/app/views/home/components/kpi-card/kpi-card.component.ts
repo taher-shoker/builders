f428261bcf9d12112443875795894016cbe12f52
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-07-23 19:19:35 +0300

    add hoverable contents to kpi-card component.

--- apps/strategic-dashboard/src/app/views/home/components/kpi-card/kpi-card.component.ts
+++ apps/strategic-dashboard/src/app/views/home/components/kpi-card/kpi-card.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  input,
+  Input,
+  InputSignal,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'stc-apps-kpi-card',
   templateUrl: './kpi-card.component.html',
   styleUrl: './kpi-card.component.scss',
 })
-export class KpiCardComponent {
+export class KpiCardComponent implements OnChanges {
   title = 'Digitize STC';
   iconPath = 'assets/images/interaction-icon.svg';
+  @Input() currentContents: any[] = [];
+  // currentContents: WritableSignal<any> = signal([]);
+  kpiTitle: InputSignal<string> = input('');
+  // activeIndex: InputSignal<number | any> = input(null);
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['currentContents']) {
+      this.currentContents = changes['currentContents'].currentValue;
+    }
+  }
+
+  get isStringContent(): boolean {
+    return typeof this.currentContents[0] === 'string';
+  }
+
+  getPercentageClass(percentage: string): string {
+    const value = parseFloat(percentage);
+    if (value < 90) {
+      return 'less-than-100';
+    } else if (value >= 90 && value <= 100) {
+      return 'near-to-100';
+    } else {
+      return 'greater-than-100';
+    }
+  }
 }
