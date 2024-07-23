cf83f18e28177c8a2d607d589c04fee227c8ad7f
Habiba Mohamed <habiba.mohamed@qeema.net>
2024-07-23 13:13:19 +0300

    adding cards in home

--- apps/strategic-dashboard/src/app/views/home/home.module.ts
+++ apps/strategic-dashboard/src/app/views/home/home.module.ts
@@ -5,8 +5,9 @@ import { HomeComponent } from './home.component';
 import { SharedModule } from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './home.routes';
+import { KpiCardComponent } from './components/kpi-card/kpi-card.component';
 
-const components = [HomeComponent, KpiCardHeaderComponent];
+const components = [HomeComponent, KpiCardHeaderComponent, KpiCardComponent];
 
 const modules = [SharedModule, RouterModule.forChild(appRoutes)];
 
