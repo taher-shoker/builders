6acec11946591336f54087d9a61d53262009c05d
Habiba Mohamed <habiba.mohamed@qeema.net>
2024-07-24 19:22:43 +0300

    adding routes for details module

--- apps/strategic-dashboard/src/app/app.routes.ts
+++ apps/strategic-dashboard/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { DetailsComponent } from './views/details/details.component';
 
 export const appRoutes: Route[] = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -11,6 +12,17 @@ export const appRoutes: Route[] = [
         path: 'home',
         loadChildren: () =>
           import('./views/home/home.module').then((m) => m.HomeModule),
+      },
+    ],
+  },
+  {
+    path: 'details/:kpiName',
+    component: DetailsComponent,
+    children: [
+      {
+        path: 'details/:kpiName',
+        loadChildren: () =>
+          import('./views/details/details.module').then((m) => m.DetailsModule),
       },
     ],
   },
