989fd0cde4f1c03bd0d62308d4e622117fb9a6bf
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-07-28 21:41:10 +0300

    complete implementation and styles of range-slider component.

--- apps/strategic-dashboard/src/app/shared/range-slider/range-slider.component.ts
+++ apps/strategic-dashboard/src/app/shared/range-slider/range-slider.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, input, InputSignal } from '@angular/core';
 @Component({
   selector: 'stc-apps-range-slider',
   templateUrl: './range-slider.component.html',
   styleUrl: './range-slider.component.scss',
 })
-export class RangeSliderComponent {}
+export class RangeSliderComponent {
+  startValue: InputSignal<number> = input(0);
+  endValue: InputSignal<number> = input(0);
+
+  formatLabel(value: number): string {
+    return value.toString() || '';
+  }
+}
