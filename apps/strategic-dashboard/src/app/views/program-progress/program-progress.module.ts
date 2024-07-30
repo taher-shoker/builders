e312f8b1ccffb66dc5426a14f2156f5678124fa6
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-07-30 17:38:42 +0300

    updates in program-progress module and routes.

--- apps/strategic-dashboard/src/app/views/program-progress/program-progress.module.ts
+++ apps/strategic-dashboard/src/app/views/program-progress/program-progress.module.ts
@@ -3,9 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ProgramProgressComponent } from './program-progress.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './program-progress.routes';
+import { ProgramDetailsComponent } from './components/program-details/program-details.component';
+import { SharedModule } from '../../shared/shared.module';
+import { ProgramDetailsCardComponent } from './components/program-details-card/program-details-card.component';
+import { DetailsModule } from '../details/details.module';
+import { KpisCardComponent } from './components/kpis-card/kpis-card.component';
 
-const components = [ProgramProgressComponent];
-const modules = [CommonModule, RouterModule.forChild(appRoutes)];
+const components = [
+  ProgramProgressComponent,
+  ProgramDetailsComponent,
+  ProgramDetailsCardComponent,
+  KpisCardComponent,
+];
+const modules = [
+  CommonModule,
+  SharedModule,
+  DetailsModule,
+  RouterModule,
+  RouterModule.forChild(appRoutes),
+];
 
 @NgModule({
   declarations: [components],
--- apps/strategic-dashboard/src/app/views/program-progress/program-progress.routes.ts
+++ apps/strategic-dashboard/src/app/views/program-progress/program-progress.routes.ts
@@ -1,9 +1,16 @@
 import { Route } from '@angular/router';
 import { ProgramProgressComponent } from './program-progress.component';
+import { ProgramDetailsComponent } from './components/program-details/program-details.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: ProgramProgressComponent,
+    children: [
+      {
+        path: 'program-details',
+        component: ProgramDetailsComponent,
+      },
+    ],
   },
 ];
