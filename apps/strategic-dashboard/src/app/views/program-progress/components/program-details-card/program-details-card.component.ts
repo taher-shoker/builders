0671da805ab35d54dece5ee251ef50ef520e8e70
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-07-30 17:36:08 +0300

    add program-details-card component.

---
+++ apps/strategic-dashboard/src/app/views/program-progress/components/program-details-card/program-details-card.component.ts
@@ -0,0 +1,70 @@
+import { Component, computed, input, InputSignal } from '@angular/core';
+import { LineChartData } from '@stc-apps/shared-ui';
+import { ProgressInfo } from 'libs/shared-ui/src/lib/progress-bar/progress-bar.component';
+
+interface Progress {
+  value: number;
+  label: string;
+  bgColor: string;
+}
+
+@Component({
+  selector: 'stc-apps-program-details-card',
+  templateUrl: './program-details-card.component.html',
+  styleUrls: ['./program-details-card.component.scss'],
+})
+export class ProgramDetailsCardComponent {
+  title: InputSignal<string> = input('');
+  chartType: InputSignal<string> = input('');
+  progressValue: InputSignal<Progress[]> = input<Progress[]>([]);
+
+  lineChartColors = ['#D2D7D9', '#45006F'];
+  lineChartData: { name: string; data: LineChartData[] }[] = [
+    {
+      name: 'Target',
+      data: [
+        { category: new Date(2022, 0, 1), value: 10 },
+        { category: new Date(2023, 1, 2), value: 60 },
+        { category: new Date(2024, 2, 3), value: 75 },
+        { category: new Date(2025, 3, 4), value: 0 },
+      ],
+    },
+    {
+      name: 'Actual',
+      data: [
+        { category: new Date(2022, 0, 1), value: 30 },
+        { category: new Date(2023, 1, 2), value: 55 },
+        { category: new Date(2024, 2, 3), value: 10 },
+        { category: new Date(2025, 3, 4), value: 25 },
+      ],
+    },
+  ];
+
+  progressBarData = computed(() => {
+    let data: ProgressInfo;
+    // eslint-disable-next-line prefer-const
+    data = {
+      prefixText: '',
+      prefixValue: 0,
+      suffixText: '',
+      suffixValue: 0,
+      progressValue: 85.0,
+      indexes: [
+        {
+          caption: 'Actual',
+          value: 93.0,
+          position: 'up',
+        },
+        {
+          caption: 'Planned',
+          value: 81.0,
+          position: 'down',
+        },
+      ],
+      barColor: '#00C48C',
+      bgBarColor: '#c82a271a',
+    };
+
+    return data;
+  });
+}
