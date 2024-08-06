09f86593ef8ffde78be4112d429e1169392073b5
Habiba Mohamed <habiba.mohamed@qeema.net>
2024-08-06 14:42:52 +0300

    adding welcomePage service

---
+++ apps/app-sector/src/app/views/welcome-page/services/welcomePage.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { environment } from 'apps/app-sector/src/environments/environment';
+import { Observable } from 'rxjs';
+import { sectorsList } from '../models/userSector.model';
+@Injectable({ providedIn: 'root' })
+export class WelcomePageService {
+  constructor(private http: HttpClient) {}
+  getUserSectors(): Observable<sectorsList> {
+    return this.http.get<sectorsList>(
+      `${environment.apiUrl}v2/scrs/dashboard/sectors`
+    );
+  }
+}
