5a9a057d36f6cb2d8de800f0fee5b43c669c2a5d
Sherif Helmy <Sherif.Helmy@qeema.net>
2024-08-08 15:30:13 +0300

    merge scorecards branch to strategy branch

--- apps/ebe/src/app/services/scorecard.service.ts
+++ apps/ebe/src/app/services/scorecard.service.ts
@@ -46,11 +46,11 @@ export class ScorecardService {
       name: 'scorecard',
       url: '/scorecard',
     },
-    // {
-    //   id: 2,
-    //   name: 'CAD strategy program',
-    //   url: '/strategy-program',
-    // },
+    {
+      id: 2,
+      name: 'CAD strategy program',
+      url: '/strategy-program',
+    },
     // {
     //   id: 3,
     //   name: 'raqami',
