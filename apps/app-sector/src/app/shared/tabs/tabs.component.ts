bd0eed142c606a9699f188000a447b18d4b99084
Habiba Mohamed <habiba.mohamed@qeema.net>
2024-08-11 19:17:46 +0300

    solving the api being called twice in tabs

--- apps/app-sector/src/app/shared/tabs/tabs.component.ts
+++ apps/app-sector/src/app/shared/tabs/tabs.component.ts
@@ -26,14 +26,21 @@ export class TabsComponent implements AfterContentInit {
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent> | any;
   @Output() changeSelectValue: EventEmitter<any> = new EventEmitter();
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   private onChange: (value: any) => void = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   private onTouched: any = () => {};
-  public disabled: boolean = false;
+  public disabled = false;
   public value: any;
 
   ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+    this.handelSelectTab();
+
     this.tabs.changes.subscribe(() => {
+      console.log('change');
       if (this.tabs.length > 0) {
+        console.log('change 2');
         this.handelSelectTab();
       }
     });
@@ -64,14 +71,15 @@ export class TabsComponent implements AfterContentInit {
   handelSelectTab() {
     const activeTabs = this.tabs?.filter((tab: TabComponent) => tab.active());
     if (activeTabs.length === 0) {
+      console.log('activeTabs is zero');
       this.selectTab(this.tabs.first);
     }
     this.tabs.forEach((tab: TabComponent) => {
       tab.selectTab.subscribe(() => {
-        // console.log(tab);
+        console.log('tab');
         this.selectTab(tab);
         this.value = tab.value();
-        this.changeValue();
+        // this.changeValue();
       });
     });
   }
@@ -80,10 +88,12 @@ export class TabsComponent implements AfterContentInit {
     const activeTabs = this.tabs?.filter(
       (tab: TabComponent) => tab.value == this.value
     );
+    // eslint-disable-next-line no-extra-boolean-cast
     if (!!activeTabs) this.selectTab(activeTabs);
   }
 
   selectTab(tab: TabComponent) {
+    console.log('selectTab');
     if (!tab) return;
     // deactivate all tabs
     this.tabs.toArray().forEach((tab: TabComponent) => tab.active.set(false));
