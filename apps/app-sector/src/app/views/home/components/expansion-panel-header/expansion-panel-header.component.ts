bc8d5a6ed47a608669eb31e9f6b6b617d42d1ba7
Habiba Mohamed <habiba.mohamed@qeema.net>
2024-08-11 19:14:59 +0300

    modifing the status styling

--- apps/app-sector/src/app/views/home/components/expansion-panel-header/expansion-panel-header.component.ts
+++ apps/app-sector/src/app/views/home/components/expansion-panel-header/expansion-panel-header.component.ts
@@ -11,4 +11,16 @@ export class ExpansionPanelHeaderComponent {
   projectHeader: InputSignal<any> = input('');
   direction: InputSignal<any> = input('');
   function: InputSignal<any> = input('');
+
+  statusBackground(status: string): string {
+    if (status.toLowerCase() === 'on track') return 'rgba(0, 196, 140, 0.15)';
+    else if (status.toLowerCase() === 'delayed')
+      return 'rgba(255, 26, 26, 0.1)';
+    else return '';
+  }
+  statusColor(status: string): string {
+    if (status.toLowerCase() === 'on track') return 'var(--stcOasisColor)';
+    else if (status.toLowerCase() === 'delayed') return 'var(--stc-red-color)';
+    else return '';
+  }
 }
