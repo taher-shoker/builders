f193e0a483444c1d2f681d31de90f97bde6c807f
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-08-12 16:06:31 +0300

    add sector guard and service.

--- apps/app-sector/src/app/app.routes.ts
+++ apps/app-sector/src/app/app.routes.ts
@@ -1,22 +1,25 @@
 import { Route } from '@angular/router';
 import { WelcomePageComponent } from './views/welcome-page/welcome-page.component';
 import { LayoutComponent } from './layout/layout.component';
+import { sectorGuard } from './services/guards/sector.guard';
 
 export const appRoutes: Route[] = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '',
     component: LayoutComponent,
     children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
       {
         path: 'home',
         loadChildren: () =>
           import('./views/home/home.module').then((m) => m.HomeModule),
+        canActivate: [sectorGuard],
       },
       {
         path: 'details/:kpiName',
         loadChildren: () =>
           import('./views/details/details.module').then((m) => m.DetailsModule),
+        canActivate: [sectorGuard],
       },
     ],
   },
