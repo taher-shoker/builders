59c21624e264319ffb98018ae54f93906d4222a0
taher shoker <taher.shoker@qeema.net>
2024-08-26 00:21:00 +0300

    fix config files for "app-sector" app

--- apps/app-sector/src/environments/environment.stage.ts
+++ apps/app-sector/src/environments/environment.stage.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'http://localhost:9084/cem/reporting/apigateway/api/',
   reportApiUrl: 'http://localhost:9084/cem/reporting/apigateway/api/auth',
   authUrl: 'http://localhost:9084',
-  languageFilesPath: '/cem/reporting/dtmilestones/assets/i18n/',
+  languageFilesPath: '/cem/reporting/score-card-report/assets/i18n/',
   loginPath: '/cem/reporting/',
 };
