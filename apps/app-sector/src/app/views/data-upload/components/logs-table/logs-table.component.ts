11a4eae13dddeb4a057a3c57c65e683c34d0516a
Habiba Mohamed <habiba.mohamed@qeema.net>
2024-08-25 20:17:05 +0300

    adding the logs-table component

---
+++ apps/app-sector/src/app/views/data-upload/components/logs-table/logs-table.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { logs } from '../../models/logModel';
+import { DataUploadService } from '../../services/data-upload.service';
+
+@Component({
+  selector: 'stc-apps-logs-table',
+  templateUrl: './logs-table.component.html',
+  styleUrl: './logs-table.component.scss',
+})
+export class LogsTableComponent implements OnInit {
+  ELEMENT_DATA: logs[] = [];
+  constructor(private dataUploadService: DataUploadService) {}
+
+  displayedColumns: string[] = [
+    'jobId',
+    'executedBy',
+    'originalFileName',
+    'startTime',
+    'endTime',
+    'status',
+    'jobCategory',
+    'talendJobName',
+    'sourceSubsidiaryName',
+  ];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  dataSource: any;
+  // ngAfterViewInit() {}
+  ngOnInit(): void {
+    this.getLogsHistory();
+  }
+  getLogsHistory() {
+    this.dataUploadService.getLogHistory().subscribe({
+      next: (result) => {
+        this.ELEMENT_DATA = result;
+        console.log(this.ELEMENT_DATA, 'dataupload');
+
+        this.dataSource = new MatTableDataSource<any>(this.ELEMENT_DATA);
+        this.dataSource.paginator = this.paginator;
+      },
+    });
+  }
+}
--- apps/app-sector/src/app/views/data-upload/data-upload.module.ts
+++ apps/app-sector/src/app/views/data-upload/data-upload.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { appRoutes } from './data-upload.routes';
 import { RouterModule } from '@angular/router';
 import { UploadFileComponent } from './data-upload.component';
 import { DataUploadTableComponent } from './components/data-upload-table/data-upload-table.component';
+import { LogsTableComponent } from './components/logs-table/logs-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
-const components = [UploadFileComponent, DataUploadTableComponent];
-const modules = [CommonModule, RouterModule.forChild(appRoutes)];
+
+const components = [UploadFileComponent, DataUploadTableComponent,LogsTableComponent];
+const modules = [CommonModule,MatTableModule,MatPaginatorModule, RouterModule.forChild(appRoutes)];
 
 @NgModule({
   declarations: [components],
