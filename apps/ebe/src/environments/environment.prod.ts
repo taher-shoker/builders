68087e35816fb4791e5ee783948cbe98bdd83b8a
Sherif Helmy <Sherif.Helmy@qeema.net>
2024-09-10 15:04:34 +0300

    hide the strategy program and psr pages

--- apps/ebe/src/environments/environment.prod.ts
+++ apps/ebe/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:9084/cem/reporting/apigateway/api/v2',
+  apiUrl: 'http://localhost:8084/cem/reporting/apigateway/api/v2',
   // apiUrl: '/cem/reporting/apigateway/api/',
   reportApiUrl: '/cem/reporting/apigateway/api/auth',
   languageFilesPath: '/cem/reporting/dtmilestones/assets/i18n/',
