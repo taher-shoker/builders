f61f003b09846a92e7b0067b3c9876383cb0e71a
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-09-12 10:24:54 +0300

    update integration in home components.

--- apps/strategic-dashboard/src/app/views/home/components/cards-holder/cards-holder.component.ts
+++ apps/strategic-dashboard/src/app/views/home/components/cards-holder/cards-holder.component.ts
@@ -14,10 +14,6 @@ export class CardsHolderComponent implements OnInit {
   activeIndex: number | null = null;
   kpis: any = {};
   strategicGroups: StrategicGroup[] = [];
-  strategicName = '';
-  private hoverSubject = new Subject<{ index: number; name: string }>();
-  private strategicNameSubject = new Subject<string>();
-  strategicGroupKpis: any[] = [];
 
   constructor(
     private strategicGroupsService: StrategicGroupsService,
@@ -38,49 +34,11 @@ export class CardsHolderComponent implements OnInit {
     this.strategicGroupsService.getAllStrategicGroups(params).subscribe(
       (result: StrategicGroup[]) => {
         this.strategicGroups = result;
-        this.loadKpisForStrategicGroups(result);
       },
       (error) => {
         console.error('Error fetching strategic groups:', error);
       }
     );
-  }
-
-  loadKpisForStrategicGroups(groups: StrategicGroup[]) {
-    const year = this.sharedFormService.getForm().controls['year'].value;
-    const kpiObservables = groups?.map((group) =>
-      this.strategicGroupsService.getAllStrategicGroupKpis({
-        year: year,
-        strategicName: group?.name,
-      })
-    );
-    forkJoin(kpiObservables)
-      .pipe(
-        // Combine all results into a single array
-        map((results) => {
-          const groupedKpis: { [key: string]: any[] } = {};
-          results.flat().forEach((kpi: any) => {
-            // Use the strategicGroupName as the key
-            const key = kpi.strategicGroupName;
-
-            if (!groupedKpis[key]) {
-              groupedKpis[key] = [];
-            }
-
-            groupedKpis[key].push(kpi);
-          });
-          return groupedKpis;
-        })
-      )
-      .subscribe(
-        (groupedKpis: { [key: string]: any[] }) => {
-          this.strategicGroupKpis.push(groupedKpis);
-          console.log(this.strategicGroupKpis);
-        },
-        (error) => {
-          console.error('Error fetching KPIs:', error);
-        }
-      );
   }
 
   getCurrentContents(index: number): any[] {
@@ -99,9 +57,8 @@ export class CardsHolderComponent implements OnInit {
     return allContents;
   }
 
-  onCardHover(index: number, strategicName: string) {
+  onCardHover(index: number) {
     this.activeIndex = index;
-    this.strategicName = strategicName;
   }
 
   onCardLeave() {
