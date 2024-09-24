b7511c9dfe15a5c7a3ece2ac24368b764cc78c2d
Habiba Mohamed <habiba.mohamed@qeema.net>
2024-09-24 13:38:21 +0300

    adjusting shared services with the year and quarter

--- apps/strategic-dashboard/src/app/shared/services/year.service.ts
+++ apps/strategic-dashboard/src/app/shared/services/year.service.ts
@@ -7,24 +7,38 @@ import { Subject } from 'rxjs';
 })
 export class YearService {
   private yearKey = 'selectedYear';
+  private quarterKey='selectedQuarter';
   private yearChangeSubject = new Subject<number>();
+  private quarterChangeSubject = new Subject<string>();
 
   constructor(private cookieService: CookieService) {}
 
-  setYear(year: string): void {
+  setYearQuarter(yearQuarter: string): void {
+    const year=yearQuarter.split('-')[0];
+    const quarter=yearQuarter.split('-')[1];
     this.cookieService.set(this.yearKey, year, { expires: 7, path: '/' });
+    this.cookieService.set(this.quarterKey, quarter, { expires: 7, path: '/' });
     this.yearChangeSubject.next(+year);
+    this.quarterChangeSubject.next(quarter);
   }
 
   getSelectedYear(): string | null {
     return this.cookieService.get(this.yearKey) || null;
   }
-
+  getSelectedQuarter(): string | null {
+    return this.cookieService.get(this.quarterKey) || null;
+  }
   clearYear(): void {
     this.cookieService.delete(this.yearKey, '/');
+  }
+  clearQuarter(): void {
+    this.cookieService.delete(this.quarterKey, '/');
   }
 
   getYearChangeObservable() {
     return this.yearChangeSubject.asObservable();
+  }
+  getQuarterChangeObservable() {
+    return this.quarterChangeSubject.asObservable();
   }
 }
