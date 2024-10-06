948c813db668a920015495e93d1c3624ffc74572
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-10-06 12:36:09 +0300

    add dashboardName param to upload api.

--- apps/strategic-dashboard/src/app/views/data-upload/components/file-upload/file-upload.component.ts
+++ apps/strategic-dashboard/src/app/views/data-upload/components/file-upload/file-upload.component.ts
@@ -9,16 +9,39 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrl: './file-upload.component.scss',
 })
 export class FileUploadComponent {
+  showList = false;
+  items = [
+    { name: 'Strategic KPIs', key: 'Strategic KPIs' },
+    { name: 'Strategic Program', key: 'Strategic Program' },
+    {
+      name: 'Strategic Program KPIs Details',
+      key: 'Strategic Program KPIs Details',
+    },
+  ];
+
+  selectedKey = '';
+
   constructor(
     private dataUploadService: DataUploadService,
     private logService: LogService
   ) {}
+
+  toggleList() {
+    this.showList = !this.showList;
+  }
+
+  onItemSelected(item: any, fileInput: HTMLInputElement) {
+    this.selectedKey = item.key;
+    fileInput.click();
+  }
+
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
+      const dashboardName = this.selectedKey;
 
-      this.dataUploadService.uploadData(file).subscribe({
+      this.dataUploadService.uploadData(file, dashboardName).subscribe({
         next: (result) => {
           this.logService.addLog(result);
         },
