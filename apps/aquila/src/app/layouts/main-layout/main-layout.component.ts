d14f7dbe096f9bf1e715aa6f9e169cae36c6a579
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-12-05 17:33:34 +0200

    add main-layout component.

---
+++ apps/aquila/src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stc-apps-main-layout',
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss'],
+})
+export class MainLayoutComponent {}
