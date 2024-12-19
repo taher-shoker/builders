078835b5d4df7b9c52a57ff8ef479569579f4d68
Sara Alkurdy <sara.alkurdy@qeema.net>
2024-12-19 11:34:37 +0200

    add table-list component.

---
+++ apps/aquila/src/app/shared/components/table-list/table-list.component.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @nx/enforce-module-boundaries */
+import { Component, computed, input } from '@angular/core';
+import { ApiStandard } from '../../models/standards.models';
+import { ColumnsSchema } from 'libs/shared-ui/src/lib/custom-table/custom-table.component';
+
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'stc-apps-table-list',
+  templateUrl: './table-list.component.html',
+  styleUrls: ['./table-list.component.scss'],
+  standalone: true,
+  imports: [TableModule, ButtonModule, CommonModule],
+})
+export class TableListComponent {
+  dataSource = input<ApiStandard[]>([]);
+  columnSchema = input<ColumnsSchema[]>([]);
+  noDataMessage = input<string>('');
+  tableActions = input<string[]>([]);
+
+  displayedColumns = computed(() => {
+    const columns = this.columnSchema();
+    if (this.tableActions().length > 0) {
+      columns.push({ key: 'actions', type: 'text', label: 'Actions' });
+    }
+    return columns;
+  });
+}
