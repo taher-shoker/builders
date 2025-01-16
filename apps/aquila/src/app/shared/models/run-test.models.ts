d6ec479a29644e17cf7ac7dc0cda985034dcc1d4
Sara Alkurdy <sara.alkurdy@qeema.net>
2025-01-16 13:03:10 +0200

    updates in services and models.

---
+++ apps/aquila/src/app/shared/models/run-test.models.ts
@@ -0,0 +1,55 @@
+export interface RunTestRequest {
+  apiUrl: string;
+  standardId: string;
+  version: string;
+}
+
+export interface RunTestResponse {
+  data: {
+    parentTestId: string;
+    ApiName: string;
+    standardName: string;
+    testStatus: string;
+    summaryFileJson: string;
+    summaryFileHtml: string;
+    result: {
+      testId: string;
+      description: string;
+      result: string;
+      recommendation: string;
+    }[];
+  };
+}
+
+export interface RunMultipleTestsResponse {
+  testResults: TestResult[];
+}
+
+export interface TestResult {
+  parentTestId: string;
+  apiName: string;
+  standardName: string;
+  testStatus: string | null;
+  summaryFileJson: string;
+  summaryFileHtml: string;
+  totalTests: number;
+  totalPasses: number;
+  totalFails: number;
+  result: string | null;
+  errorCode: string | null;
+}
+
+export interface QueueItem {
+  id: number;
+  apiUrl: string;
+  standardId: any;
+  standardList: any[];
+  domain?: string;
+  hasRun?: boolean;
+  hasCompleted?: boolean;
+  date?: Date;
+  result?: string;
+  parentTestId?: string;
+  summaryFileJson?: string;
+  summaryFileHtml?: string;
+}
---
+++ apps/aquila/src/app/shared/services/standards.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { GetStandardsResponse } from '../models/standards.models';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StandardsService {
+  private readonly apiUrl = `${environment.apiUrl}/v1/standard`;
+  private readonly http = inject(HttpClient);
+
+  getStandards(): Observable<GetStandardsResponse> {
+    return this.http.get<GetStandardsResponse>(this.apiUrl);
+  }
+
+  uploadStandard(formData: FormData): Observable<any> {
+    return this.http.post(this.apiUrl, formData, {
+      responseType: 'text',
+    });
+  }
+}
