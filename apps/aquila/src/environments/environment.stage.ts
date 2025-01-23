045f7131a8c70fc6ef21be6661560b3524e64e1f
Sara Alkurdy <sara.alkurdy@qeema.net>
2025-01-23 14:33:18 +0200

    update domain of stage environment.

--- apps/aquila/src/environments/environment.stage.ts
+++ apps/aquila/src/environments/environment.stage.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://aquila-dev.apps.qeema.io/api',
+  apiUrl: 'https://aquila-sit.apps.qeema.io/api',
   reportApiUrl:
-    'https://aquila-dev.apps.qeema.io/api/cem/reporting/apigateway/api/auth',
-  authUrl: 'https://aquila-dev.apps.qeema.io',
+    'https://aquila-sit.apps.qeema.io/api/cem/reporting/apigateway/api/auth',
+  authUrl: 'https://aquila-sit.apps.qeema.io',
   languageFilesPath: '/assets/i18n/',
   loginPath: '/login',
 };
