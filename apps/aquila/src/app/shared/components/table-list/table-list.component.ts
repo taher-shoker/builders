bf6984bef005484ef9cc27936bad85756fe7b839
Sara Alkurdy <sara.alkurdy@qeema.net>
2025-02-04 12:27:52 +0200

    updates in table-list shared component.

--- apps/aquila/src/app/shared/components/table-list/table-list.component.ts
+++ apps/aquila/src/app/shared/components/table-list/table-list.component.ts
@@ -5,6 +5,7 @@ import {
   input,
   OnInit,
   Output,
+  OnDestroy,
 } from '@angular/core';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { ColumnsSchema } from 'libs/shared-ui/src/lib/custom-table/custom-table.component';
@@ -12,9 +13,7 @@ import { ColumnsSchema } from 'libs/shared-ui/src/lib/custom-table/custom-table.
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
-import { Standard } from '../../models/standards.models';
 import { TooltipModule } from 'primeng/tooltip';
-
 @Component({
   selector: 'stc-apps-table-list',
   templateUrl: './table-list.component.html',
@@ -22,16 +21,21 @@ import { TooltipModule } from 'primeng/tooltip';
   standalone: true,
   imports: [TableModule, ButtonModule, CommonModule, TooltipModule],
 })
-export class TableListComponent implements OnInit {
-  dataSource = input<Standard[]>([]);
+export class TableListComponent implements OnInit, OnDestroy {
+  dataSource = input<any[]>([]);
   columnSchema = input<ColumnsSchema[]>([]);
   noDataMessage = input<string>('');
-  tableActions = input<{ action: string; title: string }[]>([]);
+  tableActions = input<
+    { action: string; title: string; icon?: string; tooltip?: string }[]
+  >([]);
   actionsDisabled = input<boolean>(false);
+  enablePagination = input<boolean>(true);
+  totalCount = input<number>(0);
   @Output() actionClick = new EventEmitter<{
     actionType: string;
     rowData: any;
   }>();
+  @Output() pageChange = new EventEmitter<{ page: number; pageSize: number }>();
 
   displayedColumns = computed(() => {
     const columns = this.columnSchema();
@@ -43,12 +47,19 @@ export class TableListComponent implements OnInit {
 
   currentPage = 0;
   rowsPerPage = 10;
+  totalRecords = computed(() =>
+    this.enablePagination() ? this.dataSource().length : this.totalCount()
+  );
+  first = computed(() => this.currentPage * this.rowsPerPage);
 
   ngOnInit(): void {
     const savedPage = localStorage.getItem('currentPage');
-    if (savedPage) {
-      this.currentPage = parseInt(savedPage, 10);
-    }
+    if (savedPage) this.currentPage = parseInt(savedPage, 10);
+  }
+
+  ngOnDestroy(): void {
+    localStorage.removeItem('currentPage');
+    localStorage.removeItem('rowsPerPage');
   }
 
   onActionClick(actionType: string, rowData: any): void {
@@ -59,6 +70,9 @@ export class TableListComponent implements OnInit {
 
   onPageChange(event: { first: number; rows: number }): void {
     this.currentPage = event.first / event.rows;
+    this.rowsPerPage = event.rows;
+
     localStorage.setItem('currentPage', this.currentPage.toString());
+    localStorage.setItem('rowsPerPage', this.rowsPerPage.toString());
   }
 }
