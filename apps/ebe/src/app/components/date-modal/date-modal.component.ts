eb92408599fba91457d1b5ae68d47d6713018bef
sherifhelmy <helmysherif792@gmail.com>
2025-02-05 16:17:24 +0200

    working in the design of the scorecards in mobile view

--- apps/ebe/src/app/components/date-modal/date-modal.component.ts
+++ apps/ebe/src/app/components/date-modal/date-modal.component.ts
@@ -1,11 +1,13 @@
 import { Component, input, InputSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogModule } from 'primeng/dialog';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
 type Position = "center" | "top" | "bottom" | "left" | "right" | "topleft" | "topright" | "bottomleft" | "bottomright";
 @Component({
   selector: 'stc-apps-date-modal',
   standalone: true,
-  imports: [CommonModule , DialogModule],
+  imports: [CommonModule , DialogModule , CalendarModule , FormsModule],
   templateUrl: './date-modal.component.html',
   styleUrl: './date-modal.component.scss',
 })
@@ -15,6 +17,7 @@ export class DateModalComponent {
   currentMonthName:string = '';
   visible: boolean = false;
   position!:Position;
+  date = new Date();
   months:string[] = ['Jan' , 'Feb' , 'Mar' , 'Apr' , 'May' , 'Jun' , 'Jul' , 'Aug' , 'Sep' , 'Oct' , 'Nov' , 'Dec'];
   ngOnInit()
   {
