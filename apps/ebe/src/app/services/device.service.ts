b8dd19687d7b551ee1b6e08b507831adbd2ac362
sherifhelmy <helmysherif792@gmail.com>
2025-02-16 13:27:04 +0200

    enhance the ui in mobile view

--- apps/ebe/src/app/services/device.service.ts
+++ apps/ebe/src/app/services/device.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class DeviceService {
-  private mobileRegex = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  private mobileRegex = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i; 
   constructor() {}
+  // isMobile(): boolean {
+  //   return this.mobileRegex.test(navigator.userAgent);
+  // }
   isMobile(): boolean {
-    return this.mobileRegex.test(navigator.userAgent);
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
   isDesktop(): boolean {
     return !this.isMobile();
