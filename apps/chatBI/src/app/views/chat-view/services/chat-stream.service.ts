91c9313b8b2b6aae82bf3fa780a7486626dc5f07
Mohamed Fawzy <mohamed.fawzy@qeema.net>
2025-04-21 09:33:03 +0300

    --fixing stop/start button position and handle the text input area accordigly

--- apps/chatBI/src/app/views/chat-view/services/chat-stream.service.ts
+++ apps/chatBI/src/app/views/chat-view/services/chat-stream.service.ts
@@ -11,7 +11,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class ChatStreamService {
   chunkStageSubject = new Subject<chunkData[]>();
-  baseURL = environment.apiUrl1;
+  baseURL = environment.apiUrl;
   lastStage = '';
 
   token = this.cookieService.get('token');
