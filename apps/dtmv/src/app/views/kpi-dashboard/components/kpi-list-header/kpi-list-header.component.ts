a73f9f026c9ca35df116db87cc5307c6a83b3f24
sara.alkurdy <sara.alkurdy@qeema.net>
2025-10-01 13:12:16 +0300

    feature: implement kpi list header component.

---
+++ apps/dtmv/src/app/views/kpi-dashboard/components/kpi-list-header/kpi-list-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stc-apps-kpi-list-header',
+  templateUrl: './kpi-list-header.component.html',
+  styleUrls: ['./kpi-list-header.component.scss']
+})
+export class KpiListHeaderComponent {
+
+}
